54dc26313533363aeecad4535ddd8b42f27cf8bb
Nicholas Oblitey Commey <126586864+nnocj@users.noreply.github.com>
2026-02-19 13:40:59 +0000

    df

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     outDir: "../dist",
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src/index.html"),
+        main: resolve(__dirname, "src/public/index.html"),
         //cart: resolve(__dirname, "src/cart/index.html"),
         //checkout: resolve(__dirname, "src/checkout/index.html"),
         //product: resolve(__dirname, "src/product_pages/index.html"),
